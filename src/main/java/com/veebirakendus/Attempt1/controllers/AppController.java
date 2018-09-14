e7d058b2abef3d0fec2094836a891a674f6de9dc
Fedor Stomakhin <fedor.stomakhin@gmail.com>
2018-09-14 08:56:48 +0300

    added template docs

--- src/main/java/com/veebirakendus/Attempt1/controllers/AppController.java
+++ src/main/java/com/veebirakendus/Attempt1/controllers/AppController.java
@@ -1,14 +1,14 @@
-package com.veebirakendus.Attempt1;
+package com.veebirakendus.Attempt1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AppController {
-    @RequestMapping("/")
-    @ResponseBody
+    @GetMapping("/")
     String index() {
-        return "Hello, World!";
+        return "index";
     }
 }
