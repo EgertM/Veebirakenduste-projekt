83d3f89de45c937ac3daf6d78c069616583e5802
Egert <3gertott@gmail.com>
2018-09-25 23:28:45 +0300

    Finished with all static templates except object info page; not final templates

--- src/main/java/com/veebirakendus/Attempt1/controllers/AppController.java
+++ src/main/java/com/veebirakendus/Attempt1/controllers/AppController.java
@@ -8,26 +8,53 @@ import org.springframework.web.bind.annotation.*;
 public class AppController {
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("text","Testing this place");
+        model.addAttribute("text", "Testing this place");
         return "index";
     }
+
     @GetMapping("/kontakt")
-    public String contact(Model model){
-        model.addAttribute("contact","testing this");
+    public String contact(Model model) {
+        model.addAttribute("contact", "testing this");
         return "contact";
     }
+
     @RequestMapping("/book/{id}")
-    public String getBookById(Model model, @PathVariable("id") Long id){
-        model.addAttribute("bookId",id);
+    public String getBookById(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("bookId", id);
         return "book";
     }
+
     @GetMapping("/login")
-    public String login(Model model){
-        model.addAttribute("login","login test");
+    public String login(Model model) {
+        model.addAttribute("login", "login test");
         return "login";
     }
+
     @GetMapping("/register")
-    public String register(Model model){
+    public String register(Model model) {
         return "register";
+    }
+
+    @GetMapping("/userSettings")
+    public String userSettings(Model model) {
+        return "userSettings";
+    }
+
+    @GetMapping("/kuulutus")
+    public String kuulutus(Model model) {
+        return "kuulutus";
+    }
+
+    @GetMapping("/meist")
+    public String meist(Model model){
+        return "meist";
+    }
+    @GetMapping("/kuulutused")
+    public String kuulutused(Model model){
+        return "minuKuulutused";
+    }
+    @GetMapping("/kuulutusInfo")
+    public String kuulutusInfo(Model model){
+        return "kuulutusInfo";
     }
 }
---
+++ src/main/resources/static/static.js
@@ -0,0 +1,4 @@
+$(".dropDownAd a").click(function(){
+    var newVal = $(this).text();
+    $(this).parents().find('.adButton').html(newVal);
+});
