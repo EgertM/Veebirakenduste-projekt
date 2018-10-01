6aa6f45430e611ff032bdd519f754580b7db2f43
EgertM <3gertott@gmail.com>
2018-10-01 14:37:46 +0300

    Made class for Ad object

---
+++ src/main/java/com/veebirakendus/Attempt1/entity/AdObject.java
@@ -0,0 +1,46 @@
+package com.veebirakendus.Attempt1.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AdObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String name;
+    private String description;
+    private String pictureSource;
+
+    public AdObject() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPictureSource() {
+        return pictureSource;
+    }
+
+    public void setPictureSource(String pictureSource) {
+        this.pictureSource = pictureSource;
+    }
+
+
+}
