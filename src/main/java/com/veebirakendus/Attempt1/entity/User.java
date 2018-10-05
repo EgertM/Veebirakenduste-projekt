34205b7b5477483b324916b8c59f2d3222e5109f
Fedor Stomakhin <fedor.stomakhin@gmail.com>
2018-10-05 08:56:50 +0300

    uncommented db

--- src/main/java/com/veebirakendus/Attempt1/entity/User.java
+++ src/main/java/com/veebirakendus/Attempt1/entity/User.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
     private String email;
     private String url;
     private String passwordHash; // will implement hashing externally
@@ -25,11 +25,11 @@ public class User {
 
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
