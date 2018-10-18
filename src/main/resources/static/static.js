58c416b66720f683db663131704a5cac93428ce5
EgertM <3gertott@gmail.com>
2018-10-18 22:46:58 +0300

    HTML style changes

--- src/main/resources/static/static.js
+++ src/main/resources/static/static.js
@@ -2,4 +2,9 @@ $(".dropDownAd a").click(function(){
     var newVal = $(this).text();
     $(this).parents().find('.adButton').html(newVal);
 });
-$()
+
+$(document).ready(function() {
+    setTimeout(function(){
+        $("#imagesRow").show();
+    }, 10000);
+});
