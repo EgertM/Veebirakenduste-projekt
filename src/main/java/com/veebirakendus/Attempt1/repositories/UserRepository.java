c4cea69af009167e453e2015490a6d5e79f77115
EgertM <3gertott@gmail.com>
2018-10-27 14:17:50 +0300

    trying to fix heroku crashing

--- src/main/java/com/veebirakendus/Attempt1/repositories/UserRepository.java
+++ src/main/java/com/veebirakendus/Attempt1/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.veebirakendus.Attempt1.repositories;
 
 import com.veebirakendus.Attempt1.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
