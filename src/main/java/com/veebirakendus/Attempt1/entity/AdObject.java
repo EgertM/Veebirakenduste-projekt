c7432cf7d2e4251d00f070cb4b543d8e670836d8
EgertM <3gertott@gmail.com>
2018-11-04 16:26:09 +0200

    implemented file upload

--- src/main/java/com/veebirakendus/Attempt1/entity/AdObject.java
+++ src/main/java/com/veebirakendus/Attempt1/entity/AdObject.java
@@ -8,6 +8,7 @@ public class AdObject {
     static Long lastId = 0L;
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
