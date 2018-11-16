4327e800cb7bcd95ac66b0eb9dd01f8a7afe6f1a
EgertM <3gertott@gmail.com>
2018-11-16 20:58:52 +0200

    buf fix

--- src/main/java/com/veebirakendus/Attempt1/entity/AdObject.java
+++ src/main/java/com/veebirakendus/Attempt1/entity/AdObject.java
@@ -13,7 +13,6 @@ public class AdObject {
     private Long id;
     private String name;
     private String description;
-    @Lob
     private byte[] pic;
     private String googleUid;
     private String hind;
