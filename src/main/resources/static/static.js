8bfedefd36ab46783f8631ae77974da096e16daa
EgertM <3gertott@gmail.com>
2018-11-18 22:40:22 +0200

    fixing for validators

--- src/main/resources/static/static.js
+++ src/main/resources/static/static.js
@@ -19,20 +19,3 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
 gtag('config', 'UA-128208629-1');
-
-$(document).ready(function myMap() {
-
-    var myLatLng = {lat: 58.378249, lng: 26.714673};
-    var mapProp= {
-        center: myLatLng,
-        zoom:10
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: 'Meie asukoht'
-    });
-    marker.setMap(map);
-
-});
